06a81bff548b209ce941e6ea657be11d63e8f72a
heki <52165190+chinheki@users.noreply.github.com>
2024-09-27 01:07:36 +0900

    add html

--- app/root.tsx
+++ app/root.tsx
@@ -42,20 +42,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return (
-    <html>
-      <head>
-        <link
-          rel="icon"
-          href="data:image/x-icon;base64,AA"
-        />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
+    <>
 
-        <Scripts />
-      </body>
-    </html>
+      <Outlet />
+
+      <Scripts />
+
+    </>
   );
 }
